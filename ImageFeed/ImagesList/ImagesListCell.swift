3f7c364842794ef63f9b87221454a940456ee962
uglyqurill <kirill.bryzgunov.01@mail.ru>
2023-01-08 14:15:43 +0300

    first review commit

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -1,5 +1,8 @@
 import UIKit
 
 final class ImagesListCell: UITableViewCell {
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    @IBOutlet var dateLabel: UILabel!
     static let reuseIdentifier = "ImagesListCell"
 }
