a0a34640e2a0c7d1a4e795735878c68181c2a121
uglyqurill <kirill.bryzgunov.01@mail.ru>
2023-01-25 01:09:58 +0300

    first attempt for review :)

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -1,14 +1,83 @@
 import UIKit
 
 final class ProfileViewController: UIViewController {
-    @IBOutlet var avatarImageView: UIImageView!
-    @IBOutlet var nameLabel: UILabel!
-    @IBOutlet var loginNameLabel: UILabel!
-    @IBOutlet var descriptionLabel: UILabel!
+    var labelName: UILabel?
+    var labelLogin: UILabel?
+    var labelDescription: UILabel?
     
-    @IBOutlet var logoutButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
     
-    @IBAction func didTapLogoutButton() {
+        let profileImage = UIImage(named: "userPic")
+        let profilePicture = UIImageView(image: profileImage)
+        profilePicture.translatesAutoresizingMaskIntoConstraints = false
+        
+        let labelName = UILabel()
+        labelName.text = "Екатерина Новикова"
+        labelName.textColor = .white
+        labelName.font = UIFont(name: labelName.font.fontName, size: 23) // я не разобрался, как правильно задать шрифт, подскажите пожалуйста)
+        labelName.translatesAutoresizingMaskIntoConstraints = false
+        self.labelName = labelName
+        
+        let labelLogin = UILabel()
+        labelLogin.text = "@ekaterina_nov"
+        labelLogin.textColor = .gray
+        labelLogin.font = UIFont(name: labelLogin.font.fontName, size: 13)
+        labelLogin.translatesAutoresizingMaskIntoConstraints = false
+        self.labelLogin = labelLogin
+        
+        let labelDescription = UILabel()
+        labelDescription.text = "Hello, world!"
+        labelDescription.textColor = .white
+        labelDescription.font = UIFont(name: labelDescription.font.fontName, size: 13)
+        labelDescription.translatesAutoresizingMaskIntoConstraints = false
+        self.labelDescription = labelDescription
+        
+        let exitButton = UIButton.systemButton(
+            with: UIImage(named: "logoutIcon")!,
+            target: self,
+            action: #selector(Self.didTapLogoutButton)
+        )
+        exitButton.tintColor = .red
+        exitButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        view.addSubview(profilePicture)
+        view.addSubview(labelName)
+        view.addSubview(labelLogin)
+        view.addSubview(labelDescription)
+        view.addSubview(exitButton)
+    
+        
+        NSLayoutConstraint.activate([
+            profilePicture.widthAnchor.constraint(equalToConstant: 70),
+            profilePicture.heightAnchor.constraint(equalToConstant: 70),
+            profilePicture.topAnchor.constraint(equalTo: view.topAnchor, constant: 76),
+            profilePicture.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            
+            labelName.topAnchor.constraint(equalTo: profilePicture.bottomAnchor, constant: 8),
+            labelName.leadingAnchor.constraint(equalTo: profilePicture.leadingAnchor),
+            
+            labelLogin.topAnchor.constraint(equalTo: labelName.bottomAnchor, constant: 8),
+            labelLogin.leadingAnchor.constraint(equalTo: profilePicture.leadingAnchor),
+            
+            labelDescription.topAnchor.constraint(equalTo: labelLogin.bottomAnchor, constant: 8),
+            labelDescription.leadingAnchor.constraint(equalTo: profilePicture.leadingAnchor),
+            
+            exitButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -26),
+            exitButton.centerYAnchor.constraint(equalTo: profilePicture.centerYAnchor)
+        ])
+        
+    }
+    
+    @objc
+    
+    func didTapLogoutButton() {
+        labelName?.text = "Вы вышли из профиля"
+        labelLogin?.removeFromSuperview()
+        labelDescription?.removeFromSuperview()
+        labelLogin = nil
+        labelDescription = nil
     }
 }
 
--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -6,13 +6,53 @@ final class SingleImageViewController: UIViewController {
         didSet {
             guard isViewLoaded else { return }
             imageView.image = image
+            rescaleAndCenterImageInScrollView(image: image)
         }
     }
     
     @IBOutlet var imageView: UIImageView!
+
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBAction func didTapBackButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+        let share = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        present(share, animated: true, completion: nil)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        rescaleAndCenterImageInScrollView(image: image)
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+    }
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, max(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
     }
 }
