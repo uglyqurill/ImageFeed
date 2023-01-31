2852bafc52dfc3425183ed64de6d37f9adb359ea
uglyqurill <kirill.bryzgunov.01@mail.ru>
2023-01-31 15:21:24 +0300

    настроен webView с progressView индикатором

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Кирилл Брызгунов on 26.01.2023.
+//
+
+import Foundation
+
+let AccessKey = "h9u9IDtscwhLMDSevmIPK0YXBMwvUZVfSCEbykIdB1E"
+let SecretKey = "Ip1ortflUIXhh_r0e1Qs37a01SzAor5fMsr8StqFqm0"
+let RedirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
