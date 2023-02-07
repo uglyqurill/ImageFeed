5e496553e0fd8ccdc486e9f9fbe9680a5df3e5fb
uglyqurill <kirill.bryzgunov.01@mail.ru>
2023-02-07 16:58:00 +0300

    Почти работает, но после подтверждения не переходит на экран с картинками

---
+++ ImageFeed/SplashView/SplashViewController.swift
@@ -0,0 +1,50 @@
+import UIKit
+import Foundation
+
+final class SplashViewController: UIViewController {
+    private let ShowAuthenticationScreenSegueIdentifier = "ShowAuthenticationScreen"
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if let token = OAuth2TokenStorage().token {
+    
+        } else {
+            performSegue(withIdentifier: ShowAuthenticationScreenSegueIdentifier, sender: nil)
+        }
+    }
+    
+    private func switchToTabBarController() {
+        // Получаем экземпляр `Window` приложения
+        guard let window = UIApplication.shared.windows.first else { fatalError("Invalid Configuration") }
+        
+        // Cоздаём экземпляр нужного контроллера из Storyboard с помощью ранее заданного идентификатора.
+        let tabBarController = UIStoryboard(name: "Main", bundle: .main)
+            .instantiateViewController(withIdentifier: "TabBarViewController")
+           
+        // Установим в `rootViewController` полученный контроллер
+        window.rootViewController = tabBarController
+    }
+    
+}
+
+extension SplashViewController {
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == ShowAuthenticationScreenSegueIdentifier {
+            guard
+                let navigationController = segue.destination as? UINavigationController,
+                let viewController = navigationController.viewControllers[0] as? AuthViewController
+            else { fatalError("Failed to prepare for \(ShowAuthenticationScreenSegueIdentifier)") }
+            
+            viewController.delegate = self
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
+    }
+}
+
+extension SplashViewController: AuthViewControllerDelegate {
+    
+    func authViewController(_ vc: AuthViewController, didAuthenticateWithCode code: String){
+        
+    }
+}
