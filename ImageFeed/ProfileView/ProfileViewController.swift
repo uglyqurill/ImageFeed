6d11fcd680e5dea78b2e49c5f90eacd68cc1cb38
uglyqurill <kirill.bryzgunov.01@mail.ru>
2023-03-12 20:16:19 +0300

    avatar gradient

--- ImageFeed/ProfileView/ProfileViewController.swift
+++ ImageFeed/ProfileView/ProfileViewController.swift
@@ -23,10 +23,30 @@ final class ProfileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // MARK: Gradients
+        let gradient = CAGradientLayer()
+        gradient.frame = CGRect(origin: .zero, size: CGSize(width: 70, height: 70))
+        gradient.locations = [0, 0.1, 0.3]
+        gradient.colors = [
+            UIColor(red: 0.682, green: 0.686, blue: 0.706, alpha: 1).cgColor,
+            UIColor(red: 0.531, green: 0.533, blue: 0.553, alpha: 1).cgColor,
+            UIColor(red: 0.431, green: 0.433, blue: 0.453, alpha: 1).cgColor
+        ]
+        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+        gradient.cornerRadius = 35
+        gradient.masksToBounds = true
+        //animationLayers.append(gradient)
+        profilePicture.layer.addSublayer(gradient)
+        
+        // MARK: Other code
+        
         view.backgroundColor = .ypBlack
         
         let profileImage = UIImage(named: "userPic")
         profilePicture.image = profileImage
+        profilePicture.layer.cornerRadius = 35
+        profilePicture.layer.masksToBounds = true
         profilePicture.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(profilePicture)
